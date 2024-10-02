e0b69ae425aa0fcec38547e17c8752822809f039
apoorv <apoorv1009@gmail.com>
2024-10-02 17:57:53 -0400

    add PID controller code

---
+++ pid_controller/app/src.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file src.cpp
+ * @author Apoorv Thapliyal
+ * @brief C++ source file for PID controller
+ * @version 0.1
+ * @date 2024-10-02
+ * 
+ * @copyright Copyright (c) 2024
+ * 
+ */
+
+#include "pid_lib.hpp"
+
+/**
+ * @brief Function to calculate the control input 
+ * @return float 
+ */
+float pid::PIDController::compute(float current_vel){
+    return 1.0; // Dummy return
+}
+

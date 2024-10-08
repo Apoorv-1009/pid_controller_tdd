3f8cdf8cda9170a52335d669f561b3501b7f2ebe
Apoorv Thapliyal <apoorv1009@gmail.com>
2024-10-08 19:13:24 -0400

    Add more tests

--- test/test.cpp
+++ test/test.cpp
@@ -28,4 +28,62 @@ TEST(pid_test_3, value_test) {
     EXPECT_EQ(PID.getKd(), 3.0);
 }
 
+/**
+ * @brief Test setKp function
+ * 
+ */
+TEST(pid_test_4, value_test) {
+    pid::PIDController PID(1, 1, 1, 10);
+    PID.setKp(3.0);
+    EXPECT_EQ(PID.getKp(), 3.0);
+}
+
+/**
+ * @brief Test setKi function
+ * 
+ */
+TEST(pid_test_5, value_test) {
+    pid::PIDController PID(1, 1, 1, 10);
+    PID.setKi(3.0);
+    EXPECT_EQ(PID.getKi(), 3.0);    
+}
+
+/**
+ * @brief Test setKd function
+ * 
+ */
+TEST(pid_test_6, value_test) {
+    pid::PIDController PID(1, 1, 1, 10);
+    PID.setKd(3.0);
+    EXPECT_EQ(PID.getKd(), 3.0);
+}
+
+/**
+ * @brief Test getKp function
+ * 
+ */
+TEST(pid_test_7, value_test) {
+    pid::PIDController PID(1.0, 1.0, 1.0, 10.0);
+    EXPECT_EQ(PID.getKp(), 1.0);
+}
+
+/**
+ * @brief Test getKi function
+ * 
+ */
+TEST(pid_test_8, value_test) {
+    pid::PIDController PID(1.0, 1.0, 1.0, 10.0);
+    EXPECT_EQ(PID.getKi(), 1.0);
+}
+
+/**
+ * @brief Test getKd function
+ * 
+ */
+TEST(pid_test_9, value_test) {
+    pid::PIDController PID(1.0, 1.0, 1.0, 10.0);
+    EXPECT_EQ(PID.getKd(), 1.0);
+}
+
+
 
